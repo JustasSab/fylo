140a4613435e8d705898a0ff1b8c7b636c1bd9be
JustasSab <sabaliunasjustas@gmail.com>
2022-06-09 12:36:01 +0300

    Finished Subscription component

--- src/components/subscription/Subscription.jsx
+++ src/components/subscription/Subscription.jsx
@@ -4,7 +4,28 @@ import './subscription.css';
 const Subscription = () => {
     return (
         <div className="subscription">
-            Subscription
+            <div className="subscription-content">
+                <div className="subscription-title">
+                    <h2>Get early access today</h2>
+                </div>
+                <div className="subscription-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar efficitur ipsum sed sodales. Maecenas diam sem, accumsan.</p>
+                </div>
+                <div className="subscription-bottom">
+                    <div className="subscription-form">
+                        <form>
+                            <input type="text" placeholder="email@example.com" id="email" name="email"></input>
+                        </form>
+                    </div>
+                    <div className="">
+                        <a class="" href="/">
+                            <button className="subscription-button">
+                                Get Started For Free
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
